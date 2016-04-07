bbc4a4528f6ac84e69f03f3310eff39b0b042335
BjarkeJ <bjarkeduejensen@gmail.com>
2016-04-07 19:34:51 +0200

    gulp etc.

---
+++ gulp.js
@@ -0,0 +1,29 @@
+
+var path = require('path');
+var gulp = require('gulp');
+var less = require('gulp-less');
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+
+
+var themePath = './source/theme/';
+var distPath = './dist/';
+
+
+
+/* THEME - LESS */
+gulp.task('css', function() {
+  'use strict';
+  return gulp.src(themePath + 'less/theme.less')
+    .pipe(less(
+      { paths: [ path.join(__dirname, 'less', 'includes') ] }
+    ))
+    .pipe(gulp.dest(themePath + 'css'))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest(distPath + 'css'));
+
+});
+
+gulp.task('default', [
+  'css'
+]);
